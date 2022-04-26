4826f6bc05ba705552bfb0030b32751b0710746b
youngwoo <pine951128@kakao.com>
2022-04-26 09:47:33 +0900

    coroutine, viewmodel scope, live data 라이브러리 추가

--- KBankTest/app/src/main/java/com/woojoo/kbanktest/ui/MainActivity.kt
+++ KBankTest/app/src/main/java/com/woojoo/kbanktest/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.woojoo.kbanktest
+package com.woojoo.kbanktest.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.woojoo.kbanktest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
