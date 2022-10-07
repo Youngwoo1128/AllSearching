c770f750584f1e00b8735b0193e14107acd663bb
youngwoo1128 <pine951128@hanmail.net>
2022-10-07 17:10:54 +0900

    폴더명 변경 room 연동

--- AllSearching/data/src/main/java/com/woojoo/allsearching/data/models/ResImage.kt
+++
@@ -1,6 +0,0 @@
-package com.woojoo.allsearching.data.models
-
-data class ResImage(
-    val meta: Meta,
-    val documents: ArrayList<ResDocument>
-)
--- AllSearching/data/src/main/java/com/woojoo/allsearching/data/models/ResVideo.kt
+++
@@ -1,6 +0,0 @@
-package com.woojoo.allsearching.data.models
-
-data class ResVideo(
-    val meta: Meta,
-    val documents: ArrayList<ResDocument>,
-)
