09e30bcc4d488dc3b3c93216d7f664bec73bbadd
youngwoo1128 <pine951128@hanmail.net>
2022-11-11 15:46:42 +0900

    Room Insert 버그 현상 기록

--- AllSearching/data/src/main/java/com/woojoo/allsearching/data/local/ResearchingDao.kt
+++ AllSearching/data/src/main/java/com/woojoo/allsearching/data/local/ResearchingDao.kt
@@ -13,7 +13,9 @@ interface ResearchingDao {
     @Query("SELECT * FROM ${ResearchingEntity.TABLE_NAME}")
     suspend fun getAll(): List<ResearchingEntity>
 
-    //중복 insert 방지
+    // 중복 insert 방지 : 현재 문제 : 동일한 값을 Insert 해도 Insert 가 가능함
+    // 이유 -> onConflict 의 기준은 다름 아닌 Id 었다,,,
+    // 내용은 같아도,,, id만 다르기 때문 이었다,,
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertResearching(item: ResearchingEntity)
 
