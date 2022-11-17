b3ece507f23b95cdf1de4b1c455fdbfbb2c3f296
youngwoo1128 <pine951128@hanmail.net>
2022-11-17 17:52:19 +0900

    보완해야할 점 추가

--- AllSearching/app/src/main/java/com/woojoo/allsearching/extension/ExtCoroutineResquest.kt
+++ AllSearching/app/src/main/java/com/woojoo/allsearching/extension/ExtCoroutineResquest.kt
@@ -8,7 +8,8 @@ import kotlinx.coroutines.launch
 // 개선 해야할 점
 // 1. exception handling 하는 것이 필요함
 // 2. 사실 presentor 영역에 있을 필요가 없는 코드. 여러 레퍼런스를 찾아보고 연구해본 결과 clean Architecture 에 따르면
-// data 영역에 들어가는 것이 맞다고 판단.
+// data 영역에 들어 가는 것이 맞다고 판단.
+// 때문에 이를 보완하는 작업 시작
 fun CoroutineScope.requestAPI(
     block: suspend CoroutineScope.() -> Unit
 ) {
