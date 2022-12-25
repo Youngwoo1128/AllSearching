2a387ea9b08b5345d5718387fe77aae2c1753934
WooJoo <pine951128@hanmail.net>
2022-12-25 10:30:58 +0900

    코드정리

--- AllSearching/data/src/main/java/com/woojoo/allsearching/data/network/HeaderInterceptor.kt
+++ AllSearching/data/src/main/java/com/woojoo/allsearching/data/network/HeaderInterceptor.kt
@@ -12,35 +12,37 @@ class HeaderInterceptor @Inject constructor(
 ) : Interceptor {
 
     private fun hasNetwork(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetwork = connectivityManager.activeNetworkInfo
         return activeNetwork != null && activeNetwork.isConnected
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        var request = chain.request()
-        if (request.header("Cache-control") == null) {
-            request = if (hasNetwork(context)) {
-                request
-                    .newBuilder()
-                    .header("Cache-control", "public, max-age=5") // 5초
-                    .header("Host", "dapi.kakao.com")
-                    .header("Authorization", "KakaoAK $KAKAO_REST_API_KEY")
-                    .build()
-            } else {
-                request
-                    .newBuilder()
-                    .header("Cache-control", "public, only-if-cached, max-stale=${60 * 60 * 24 * 7}") // 일주일
-                    .header("Host", "dapi.kakao.com")
-                    .header("Authorization", "KakaoAK $KAKAO_REST_API_KEY")
-                    .build()
-            }
+        val request = chain.request()
+        val headerInfo = if (hasNetwork(context)) {
+            request
+                .newBuilder()
+                .header("Cache-control", "public, max-age=$CACHING_TIME") // 5초
+                .header("Host", "dapi.kakao.com")
+                .header("Authorization", "KakaoAK $KAKAO_REST_API_KEY")
+                .build()
+        } else {
+            request
+                .newBuilder()
+                .header("Cache-control", "public, only-if-cached, max-stale=$CACHING_PERIOD") // 일주일
+                .header("Host", "dapi.kakao.com")
+                .header("Authorization", "KakaoAK $KAKAO_REST_API_KEY")
+                .build()
         }
-        return chain.proceed(request)
+
+        return chain.proceed(headerInfo)
     }
 
     companion object {
         private const val KAKAO_REST_API_KEY = "97e6e3cc8b4ae9a058df4f56ccd8844b"
+        private const val CACHING_TIME = 60 * 5 //5분
+        private const val CACHING_PERIOD = 60 * 60 * 24 * 7 //1주일
     }
 
 }
